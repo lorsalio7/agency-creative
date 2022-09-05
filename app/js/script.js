d9e9d09ba19df0bcdf4816847691347b693c1f97
Lorsalio <lorsalio7@gmail.com>
2022-09-05 15:39:14 +0500

    Добавлена библиотека 'AOS-JS' и установлена анимация

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-let a = 9;
+AOS.init({
+  once: true
+});
+
+// const portfolioCards = document.querySelectorAll(".portfolio__item");
+
+// portfolioCards.forEach(card => {
+//   card.style.transition = "transform  ease-in-out";
+//   card.addEventListener("mouseenter", (e) => {
+//     let targetCard = e.target;
+
+//     targetCard.style.transform = "scale(1.02)";
+//   });
+
+//   card.addEventListener("mouseleave", (e) => {
+//     let targetCard = e.target;
+
+//     targetCard.style.transform = "scale(1)";
+//   })
+// })
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-var a = 9;
+AOS.init({
+  once: true
+}); // const portfolioCards = document.querySelectorAll(".portfolio__item");
+// portfolioCards.forEach(card => {
+//   card.style.transition = "transform  ease-in-out";
+//   card.addEventListener("mouseenter", (e) => {
+//     let targetCard = e.target;
+//     targetCard.style.transform = "scale(1.02)";
+//   });
+//   card.addEventListener("mouseleave", (e) => {
+//     let targetCard = e.target;
+//     targetCard.style.transform = "scale(1)";
+//   })
+// })

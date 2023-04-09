ed73f517ed68b78a8f3e365d0e46c4ce40dcba30
Lorsalio7 <lorsalio7@gmail.com>
2023-04-09 16:41:48 +0500

    Исправлен прыжок страницы при открытии бургер меню, и улучшена навигация

--- app/js/script.js
+++ app/js/script.js
@@ -3,20 +3,88 @@ AOS.init({
 });
 
 const burgerButton = document.querySelector(".burger-button");
-const siteNavigationOverlay = document.querySelector(".main-header__overlay");
+
+function removeActiveClass(elements, activeClass) {
+  elements.forEach(element => {
+    element.classList.remove(activeClass);
+  });
+}
+
+const burgerMenuWidth = window.matchMedia("(max-width: 920px)");
 const siteNavigation = document.querySelector(".main-header__navigation");
-const html = document.querySelector(".index-html");
+
+if(siteNavigation) {
+  const siteNavigationLinks = siteNavigation.querySelectorAll(".site-navigation__link");
+
+  siteNavigationLinks.forEach(link => {
+
+    link.addEventListener("click", (e) => {
+      removeActiveClass(siteNavigationLinks, "site-navigation__link--active");
+      e.target.classList.add("site-navigation__link--active");
+
+      if(siteNavigation.classList.contains("main-header__navigation--active")) {
+        closeSiteNavigation();
+      }
+    })
+  })
+}
+
+
+if(burgerButton) {
+
+  burgerButton.addEventListener("click", () => {
+    burgerButton.classList.toggle("burger-button--active");
+
+    if(burgerButton.classList.contains("burger-button--active")) {
+      openSiteNavigation();
+    } else {
+      closeSiteNavigation();
+    }
+  })
+
+}
 
 
 function openSiteNavigation() {
-  html.classList.toggle("no-scroll");
-  burgerButton.classList.toggle("burger-button--active");
-  siteNavigation.classList.toggle("main-header__navigation--active");
-  siteNavigationOverlay.classList.toggle("main-header__overlay--active");
+  const siteNavigationOverlay = document.querySelector(".main-header__overlay");
+  const html = document.querySelector(".index-html");
+
+  let widthScroll = window.innerWidth - document.body.offsetWidth + "px";
+  html.classList.add("no-scroll");
+  siteNavigation.classList.add("main-header__navigation--active");
+  siteNavigationOverlay.classList.add("main-header__overlay--active");
+
+  document.body.style.paddingRight = widthScroll;
+  if(siteNavigationOverlay.classList.contains("main-header__overlay--active")) {
+    siteNavigationOverlay.addEventListener("click", closeSiteNavigation);
+  }
 }
 
-burgerButton.addEventListener("click", openSiteNavigation);
+function closeSiteNavigation() {
+  const siteNavigationOverlay = document.querySelector(".main-header__overlay");
+  const html = document.querySelector(".index-html");
+  burgerButton.classList.remove("burger-button--active");
+  html.classList.remove("no-scroll");
+  siteNavigation.classList.remove("main-header__navigation--active");
+  siteNavigationOverlay.classList.remove("main-header__overlay--active");
+  document.body.style.paddingRight = 0;
+}
 
+document.addEventListener("keydown", (e) => {
+  if(siteNavigation.classList.contains("main-header__navigation--active") && e.keyCode === 27) {
+    closeSiteNavigation();
+  }
+});
+
+function changeMenuView(width) {
+  if(!width) {
+    closeSiteNavigation();
+  }
+}
+
+burgerMenuWidth.onchange = (e) => {
+  changeMenuView(e.matches);
+}
 
 const slider = document.querySelector(".swiper");
 
--- dist/js/script.js
+++ dist/js/script.js
@@ -4,18 +4,82 @@ AOS.init({
   once: true
 });
 var burgerButton = document.querySelector(".burger-button");
-var siteNavigationOverlay = document.querySelector(".main-header__overlay");
-var siteNavigation = document.querySelector(".main-header__navigation");
-var html = document.querySelector(".index-html");
 
-function openSiteNavigation() {
-  html.classList.toggle("no-scroll");
-  burgerButton.classList.toggle("burger-button--active");
-  siteNavigation.classList.toggle("main-header__navigation--active");
-  siteNavigationOverlay.classList.toggle("main-header__overlay--active");
+function removeActiveClass(elements, activeClass) {
+  elements.forEach(function (element) {
+    element.classList.remove(activeClass);
+  });
 }
 
-burgerButton.addEventListener("click", openSiteNavigation);
+var burgerMenuWidth = window.matchMedia("(max-width: 920px)");
+var siteNavigation = document.querySelector(".main-header__navigation");
+
+if (siteNavigation) {
+  var siteNavigationLinks = siteNavigation.querySelectorAll(".site-navigation__link");
+  siteNavigationLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      removeActiveClass(siteNavigationLinks, "site-navigation__link--active");
+      e.target.classList.add("site-navigation__link--active");
+
+      if (siteNavigation.classList.contains("main-header__navigation--active")) {
+        closeSiteNavigation();
+      }
+    });
+  });
+}
+
+if (burgerButton) {
+  burgerButton.addEventListener("click", function () {
+    burgerButton.classList.toggle("burger-button--active");
+
+    if (burgerButton.classList.contains("burger-button--active")) {
+      openSiteNavigation();
+    } else {
+      closeSiteNavigation();
+    }
+  });
+}
+
+function openSiteNavigation() {
+  var siteNavigationOverlay = document.querySelector(".main-header__overlay");
+  var html = document.querySelector(".index-html");
+  var widthScroll = window.innerWidth - document.body.offsetWidth + "px";
+  html.classList.add("no-scroll");
+  siteNavigation.classList.add("main-header__navigation--active");
+  siteNavigationOverlay.classList.add("main-header__overlay--active");
+  document.body.style.paddingRight = widthScroll;
+
+  if (siteNavigationOverlay.classList.contains("main-header__overlay--active")) {
+    siteNavigationOverlay.addEventListener("click", closeSiteNavigation);
+  }
+}
+
+function closeSiteNavigation() {
+  var siteNavigationOverlay = document.querySelector(".main-header__overlay");
+  var html = document.querySelector(".index-html");
+  burgerButton.classList.remove("burger-button--active");
+  html.classList.remove("no-scroll");
+  siteNavigation.classList.remove("main-header__navigation--active");
+  siteNavigationOverlay.classList.remove("main-header__overlay--active");
+  document.body.style.paddingRight = 0;
+}
+
+document.addEventListener("keydown", function (e) {
+  if (siteNavigation.classList.contains("main-header__navigation--active") && e.keyCode === 27) {
+    closeSiteNavigation();
+  }
+});
+
+function changeMenuView(width) {
+  if (!width) {
+    closeSiteNavigation();
+  }
+}
+
+burgerMenuWidth.onchange = function (e) {
+  changeMenuView(e.matches);
+};
+
 var slider = document.querySelector(".swiper");
 var mySwiper;
 
